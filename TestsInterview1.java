fdfddb4096a5e445cea9c00bf8b0e816653bbc1b
Vitalii <besteffects@gmail.com>
2019-10-07 00:57:46 -0400

    Fix of checkNewspaperWords

--- TestsInterview1.java
+++ TestsInterview1.java
@@ -3,17 +3,17 @@ package Tests;
 public class TestsInterview {
 	
 	public static void main(String[] args) {
-		System.out.println(ZapiskaIGazeta("abc, de! fg.","de? fg!")); 
+		System.out.println(checkNewspaperWords("abc, de! fg.","de? fg!"));
 	}
+
+
+//	1. There is a scoundrel who stole a dog from a neighbor.
+//	He wants to write a ransom note, cutting out words from a newspaper.
+//	The task is to write a function that accepts 2 strings (text of the newspaper, text of the note)
+//	and returns true if all the words from the note are in the text of the newspaper or false if there are not enough words
+// cover case when words in note more than in newspaper
 	
-	
-//	1. Есть негодяй, который украл собаку у соседа.
-//	Он хочет написать записку о выкупе, вырезая слова из газеты. 
-//	Задание написать функцию, которая принимает 2 стринга (текст газеты, текст записки) 
-//	и возвращает true, если все слова из записки есть в тексте газеты или false, 
-//	если каких-то слов не хватает
-	
-	public static boolean ZapiskaIGazeta(String newspp, String note) {
+	public static boolean checkNewspaperWords(String newspp, String note) {
 		boolean result = false;
 		int count = 0;
 		String[] newspWords = newspp.split("\\W+");
