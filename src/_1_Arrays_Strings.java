f0ed394d18bd11a6a241a15b5309ce697ab0fa95
Vit <besteffects@gmail.com>
2020-06-28 02:00:11 -0400

    CheckPermutation

--- src/_1_Arrays_Strings.java
+++ src/_1_Arrays_Strings.java
@@ -1,11 +1,12 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class _1_Arrays_Strings {
-    /*
-   1 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
-     */
+
     public static void main(String[] args) {
+        // 1 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
         String unique1 = "12345";
         String unique2 = "Noot";
         String unique3 = "ASDFGA";
@@ -20,6 +21,11 @@ public class _1_Arrays_Strings {
         System.out.println(isUnique2(unique1));
         System.out.println(isUnique2(unique2));
         System.out.println(isUnique2(unique3));
+
+        //2 Given two strings, write a method to decide if one is a permutation of the other
+        String permut1 = "ABCD";
+        String permut2 = "ACDB";
+        System.out.println("Check is a string a is a permutation of a string b: " +CheckPermutation(permut1,permut2));
     }
 
     //1
@@ -59,13 +65,27 @@ public class _1_Arrays_Strings {
         char[] chars = name.toCharArray();
         Set<Character> set = new HashSet<>();
         for (int i = 0; i < chars.length; i++) {
-            if (!set.contains(chars[i])){
+            if (!set.contains(chars[i])) {
                 set.add(chars[i]);
-            }
-            else return false;
+            } else return false;
         }
         return true;
+    }
 
+
+    // 2
+    //a Sort the string
+    static String sort(String s) {
+        char[] content = s.toCharArray();
+        Arrays.sort(content);
+        return new String(content);
+    }
+
+    static boolean CheckPermutation(String first, String second) {
+        if (first.length() != second.length()) {
+            return false;
+        }
+        return sort(first).equals(sort(second));
     }
 }
 
