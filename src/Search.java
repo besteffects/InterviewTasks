6264e4eed53a45d080f357282eada161807ec915
Vit <besteffects@gmail.com>
2020-06-30 10:58:09 -0400

    binarySearch

---
+++ src/Search.java
@@ -0,0 +1,24 @@
+public class Search {
+    private static int binarySearchValue;
+    private static int[] arrayBinary;
+
+    public static void main(String[] args) {
+        binarySearch(binarySearchValue, arrayBinary);
+    }
+
+    static void binarySearch(int value, int[] array) {
+        int lowIndex = 0;
+        int highIndex = array.length - 1;
+        while (lowIndex <= highIndex) {
+            int middleIndex = (highIndex + lowIndex) / 2;
+            if (array[middleIndex] < value) {
+                lowIndex = middleIndex;
+            } else if (array[middleIndex] > value) {
+                highIndex = middleIndex - 1;
+            } else {
+                System.out.println("\nFound a match for " + value + "at Index " + middleIndex);
+                lowIndex = highIndex + 1;
+            }
+        }
+    }
+}
--- src/_1_Arrays_Strings.java
+++ src/_1_Arrays_Strings.java
@@ -25,21 +25,24 @@ public class _1_Arrays_Strings {
         //2 Given two strings, write a method to decide if one is a permutation of the other
         String permut1 = "ABCD";
         String permut2 = "ACDB";
-        System.out.println("Check is a string a is a permutation of a string b: " +CheckPermutation(permut1,permut2));
+        System.out.println("Check is a string a is a permutation of a string b: " + CheckPermutation(permut1, permut2));
 
         /* 3 Write a method to replace of spaces in a string with '%20. You may assume that the string has sufficient space
         at the end to hold the additional characters and that you are given true length of the string.
          */
-        urlify("Mr John Smith", 13);
+         urlify("Mr John Smith    ", 13);
 
     }
 
     //1
     static boolean isUnique(String input) {
+        //ASCII string
         if (input.length() > 128) {
             return false;
         }
         boolean[] inputChar = new boolean[128];
+        //flag at index i indicates whether character i in the alphabet is contained in the string.
+        //The second time you see the character, you immediately return false.
         for (int i = 0; i < input.length(); i++) {
             int val = input.charAt(i);
             if (inputChar[val]) {
@@ -84,9 +87,10 @@ public class _1_Arrays_Strings {
     static String sort(String s) {
         char[] content = s.toCharArray();
         Arrays.sort(content);
-        return  new String(content);
+        return new String(content);
     }
-//b compare lengths of two strings
+
+    //b compare lengths of two strings
     static boolean CheckPermutation(String first, String second) {
         if (first.length() != second.length()) {
             return false;
@@ -95,52 +99,55 @@ public class _1_Arrays_Strings {
     }
 
     //2b
-    static boolean permutation1 (String s, String t){
-        if (s.length()!=t.length()){
+    static boolean permutation1(String s, String t) {
+        if (s.length() != t.length()) {
             return false;
         }
         int[] letters = new int[128]; //Assumption
-        char [] s_array = s.toCharArray();
-        for (char c :s_array){ //count number of each char in s.
+        char[] s_array = s.toCharArray();
+        for (char c : s_array) { //count number of each char in s.
             letters[c]++;
         }
 //compare two arrays
-        for (int i = 0; i <t.length() ; i++) {
-            int c= (int)t.charAt(i);
+        for (int i = 0; i < t.length(); i++) {
+            int c = (int) t.charAt(i);
             letters[c]--;
-            if (letters[c]<0){
+            if (letters[c] < 0) {
                 return false;
             }
         }
         return true;
     }
-
-    static void urlify(String str, int trueLength){
-        char[] strArray= str.toCharArray();
+//3
+    static void urlify(String str, int trueLength) {
+        char[] strArray = str.toCharArray();
         //in the first scan we count the number of spaces
-        int spaceCount =0, index,  i=0;
-        for (i = 0; i <trueLength ; i++) {
-            if (strArray[i]==' '){
+        int spaceCount = 0, index, i = 0;
+        for (i = 0; i < trueLength; i++) {
+            if (strArray[i] == ' ') {
                 spaceCount++;
             }
         }
-        index= trueLength+spaceCount*2;
+        //here we compute how many extra characters will be required for the final string
+        index = trueLength + spaceCount * 2;
         if (trueLength < strArray.length) {
-            strArray[trueLength]='\0'; //End array
+            strArray[trueLength] = '\0'; //End array (cut off everything after the first n characters)
         }
-
-        for (i=trueLength-1;i>=0; i--){
-            if(strArray[i]== ' '){
-                strArray[index-1]='0';
-                strArray[index-2]='2';
-                strArray[index-3]='%';
-                index= index-3;
+    //here we replace the space with %20
+        for (i = trueLength - 1; i >= 0; i--) {
+            if (strArray[i] == ' ') {
+                strArray[index - 1] = '0';
+                strArray[index - 2] = '2';
+                strArray[index - 3] = '%';
+                index = index - 3;
             }
+            //if there is not space - we copy the original value
             else {
-                strArray[index-1]= strArray[i];
+                strArray[index - 1] = strArray[i];
                 index--;
             }
         }
+        System.out.println(strArray);
     }
 
 
