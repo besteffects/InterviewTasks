979abde288cf32db19ca90b81751a0b7974a0ddd
Vit <besteffects@gmail.com>
2020-07-02 11:01:29 -0400

    isPermutationOfPalindrome

--- src/_1_Arrays_Strings.java
+++ src/_1_Arrays_Strings.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public class _1_Arrays_Strings {
 
     public static void main(String[] args) {
-        // 1 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+        //  1 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
         String unique1 = "12345";
         String unique2 = "Noot";
@@ -30,8 +30,14 @@ public class _1_Arrays_Strings {
         /* 3 Write a method to replace of spaces in a string with '%20. You may assume that the string has sufficient space
         at the end to hold the additional characters and that you are given true length of the string.
          */
-         urlify("Mr John Smith    ", 13);
+        urlify("Mr John Smith    ", 13);
 
+
+         /* 4 Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase
+         the is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be
+         limited to just dictionary words.
+         To be a permutation of a palindrome, a string can have no more than one character that is odd. This will cover both the odd and the even cases.*/
+        System.out.println("Is this phrase a permutation of palindrome? " + isPermutationOfPalindrome("Tact Coa"));
     }
 
     //1
@@ -118,7 +124,8 @@ public class _1_Arrays_Strings {
         }
         return true;
     }
-//3
+
+    //3
     static void urlify(String str, int trueLength) {
         char[] strArray = str.toCharArray();
         //in the first scan we count the number of spaces
@@ -133,8 +140,8 @@ public class _1_Arrays_Strings {
         if (trueLength < strArray.length) {
             strArray[trueLength] = '\0'; //End array (cut off everything after the first n characters)
         }
-    //here we replace the space with %20
-        for (i = trueLength - 1; i >= 0 &&index>2; i--) { //added && index>2
+        //here we replace the space with %20
+        for (i = trueLength - 1; i >= 0 && index > 2; i--) { //added && index>2
             if (strArray[i] == ' ') {
                 strArray[index - 1] = '0';
                 strArray[index - 2] = '2';
@@ -150,6 +157,52 @@ public class _1_Arrays_Strings {
         System.out.println(strArray);
     }
 
+    // 4 We use a hash table to count how many times each character appears. Then, we iterate through the hash table and ensure
+    //that no more than one character has an odd count
+    public static boolean isPermutationOfPalindrome(String phrase) {
+//4a Build hash table
+        int[] table = buildCharFrequencyTable(phrase);
+        return checkMaxOneOdd(table);
+    }
 
+    //4c Check that no more than one character has an odd count
+    static boolean checkMaxOneOdd(int[] table) {
+        boolean foundOdd = false;
+        for (int count : table) {
+            if (count % 2 == 1) {
+                if (foundOdd) {  //same as foundOdd==true
+                    return false;
+                }
+                foundOdd = true;
+            }
+        }
+        return true;
+    }
+
+    //4a Map each character to a number. a->0, b->1, c->2, etc
+    //This is case insensitive. Non-letter characters map to -1
+
+    static int getCharNumber(Character c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+        if (a <= val && val <= z) {
+            return val - a;
+        }
+        return -1;
+    }
+
+    //4b Count how many times each character appears
+    static int[] buildCharFrequencyTable(String phrase) {
+        int[] table = new int[Character.getNumericValue('z') -
+                Character.getNumericValue('a') + 1];
+        for (char c : phrase.toCharArray()) {
+            int x = getCharNumber(c);
+            if (x != -1) {
+                table[x]++;
+            }
+        }
+        return table;
+    }
 }
 
