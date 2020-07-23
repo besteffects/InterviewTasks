e7c97c1afea4febffeef15078455bc575464d1e2
Vit <besteffects@gmail.com>
2020-07-23 16:52:02 -0400

    oneEditAway1

--- src/_1_Arrays_Strings/OneWay.java
+++ src/_1_Arrays_Strings/OneWay.java
@@ -14,7 +14,7 @@ Removal: The strings apple and aple are also one removal away, since removal is 
 */
 public class OneWay {
     public static void main(String[] args) {
-  //      oneEditAway("pale", "bale"); //test for replace
+        //      oneEditAway("pale", "bale"); //test for replace
         oneEditAway("pales", "pale"); //test for insert/remove
     }
 
@@ -22,15 +22,15 @@ public class OneWay {
     static boolean oneEditAway(String first, String second) {
         if (first.length() == second.length()) {
             return oneEditReplace(first, second);
-        } else if (first.length() + 1 == second.length()) {
+        } else if (first.length() + 1 == second.length()) { //insertion
             return oneEditInsert(first, second);
-        } else if (first.length() - 1 == second.length()) {
+        } else if (first.length() - 1 == second.length()) { //removal
             return oneEditInsert(second, first);
         }
         return false;
     }
 
-   static private boolean oneEditReplace(String s1, String s2) {
+    static private boolean oneEditReplace(String s1, String s2) {
         boolean foundDifference = false;
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
@@ -44,7 +44,7 @@ public class OneWay {
     }
 
     //Check if you can insert a character into s1 to make s2
-   static private boolean oneEditInsert(String s1, String s2) {
+    static private boolean oneEditInsert(String s1, String s2) {
         int index1 = 0;
         int index2 = 0;
         while (index2 < s2.length() && index1 < s1.length()) {
@@ -56,6 +56,37 @@ public class OneWay {
             } else {
                 index1++;
                 index2++;
+            }
+        }
+        return true;
+    }
+
+    //compare each character and ensure that the strings are only different by one
+    private static boolean oneEditAway1(String first, String second) {
+        //Length checks
+        if (Math.abs(first.length() - second.length()) > 1) {
+            return false;
+        }
+        //Get shorter and longer string
+        String s1 = first.length() < second.length() ? first : second; //shorter string - first, second
+        String s2 = first.length() < second.length() ? second : first; //longer string -second,first
+
+        int index1 = 0;
+        int index2 = 0;
+
+        boolean foundDifference = false;
+        while (index2 < s2.length() && index1 < s1.length()) {
+            if (s1.charAt(index1) != s2.charAt(index2)) {
+//Ensure that this is the first difference found
+                if (foundDifference)
+                    return false;
+                foundDifference = true;
+
+                if (s1.length() == s2.length()) { //On replace, move shorter pointer
+                    index1++;//If matching, move shorter pointer
+                }
+            } else {
+                index2++;//Always move pointer for longer string
             }
         }
         return true;
