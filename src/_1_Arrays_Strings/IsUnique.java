ed1303f775716dd72505ff9e3fb3cf508c399733
Vit <besteffects@gmail.com>
2020-07-23 22:58:50 -0400

    Convert char to ascii

--- src/_1_Arrays_Strings/IsUnique.java
+++ src/_1_Arrays_Strings/IsUnique.java
@@ -10,9 +10,11 @@ public class IsUnique {
         String unique1 = "12345";
         String unique2 = "Noot";
         String unique3 = "ASDFGA";
-//        System.out.println(isUnique(unique1));
-//        System.out.println(isUnique(unique2));
-//        System.out.println(isUnique(unique3));
+//      System.out.println(isUnique(unique1));
+        System.out.println(isUnique(unique2));
+//      System.out.println(isUnique(unique3));
+
+        System.out.println(isUnique1a(unique2));
 //
 //        System.out.println(isUnique1(unique1));
 //        System.out.println(isUnique1(unique2));
@@ -22,12 +24,12 @@ public class IsUnique {
 //        System.out.println(isUnique2(unique2));
 //        System.out.println(isUnique2(unique3));
 
-        System.out.println(isUnique3(unique1));
-        System.out.println(isUnique3(unique2));
-        System.out.println(isUnique3(unique3));
+        //  System.out.println(isUnique3(unique1));
+        // System.out.println(isUnique3(unique2));
+        //  System.out.println(isUnique3(unique3));
     }
 
-    //1
+    //1st solution
     static boolean isUnique(String input) {
         //ASCII string
         if (input.length() > 128) {
@@ -37,7 +39,7 @@ public class IsUnique {
         //flag at index i indicates whether character i in the alphabet is contained in the string.
         //The second time you see the character, you immediately return false.
         for (int i = 0; i < input.length(); i++) {
-            int val = input.charAt(i);
+            int val = input.charAt(i); //convert char to ASCII !!
             if (inputChar[val]) {
                 return false;
             }
@@ -46,7 +48,8 @@ public class IsUnique {
         return true;
     }
 
-    //1a On2 Compare every character of the string to every other character of the string
+
+    //2nd a) solution On2 Compare every character of the string to every other character of the string
     static boolean isUnique1(String name) {
         char[] array = name.toCharArray();
         // If at any time we encounter 2 same
@@ -62,7 +65,8 @@ public class IsUnique {
         // return true
         return true;
     }
-    //1b Implementation with hashset (contains only unique values)
+
+    //2 b) Implementation with hashset (contains only unique values)
     static boolean isUnique2(String name) {
         char[] chars = name.toCharArray();
         Set<Character> set = new HashSet<>();
@@ -73,18 +77,37 @@ public class IsUnique {
         }
         return true;
     }
-    //1c Sort the string and then linearly check the string for neighboring characters that are identical.
+
+    //3  Sort the string and then linearly check the string for neighboring characters that are identical.
     static boolean isUnique3(String name) {
-        boolean isUnique=false;
+        boolean isUnique = false;
         char[] array = name.toCharArray();
         Arrays.sort(array);
-        for (int i = 0; i < array.length-1; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             if (array[i] == array[i + 1]) {
-                isUnique=false;
+                isUnique = false;
                 break;
-            }
-            else isUnique=true;
+            } else isUnique = true;
         }
         return isUnique;
+    }
+
+    //1a  solution with toCharArray
+    static boolean isUnique1a(String input) {
+        //ASCII string
+        if (input.length() > 128) {
+            return false;
+        }
+        boolean[] booleanChar = new boolean[128];
+        //flag at index i indicates whether character i in the alphabet is contained in the string.
+        //The second time you see the character, you immediately return false.
+        char[] stringChar = input.toCharArray();
+        for (int i = 0; i < stringChar.length; i++) {
+            if (booleanChar[stringChar[i]]) {
+                return false;
+            }
+            booleanChar[stringChar[i]] = true;
+        }
+        return true;
     }
 }
