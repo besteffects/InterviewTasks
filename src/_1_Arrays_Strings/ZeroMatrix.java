b5b7976a05544ab835ddae5b873f372d4e0585a5
Vit <besteffects@gmail.com>
2020-07-28 01:14:28 -0400

    Second way of setZeros1

--- src/_1_Arrays_Strings/ZeroMatrix.java
+++ src/_1_Arrays_Strings/ZeroMatrix.java
@@ -52,4 +52,69 @@ public class ZeroMatrix {
         }
     }
 
+    //2 Second way.
+    /* To make it more efficient we can use a bit vector instead of a boolean array. It would still be 0(1) space.
+    We can reduce the space to O(1) by using the first row as a replacement for the row array and the first column as a replacement
+    for the column array. This works as follows:
+    1. Check if the first row and first column have any zeros, and set variables rowHasZero and columnHasZero. (We'll nullify the first
+    row and first column later, if necessary.)
+    2. Iterate through the rest of the matrix, setting matrix[i][0] and matrix[0][j] to zero whenever there's a zero matrix[i][j].
+    3. Iterate through the rest of matrix, nullifying row i if there's a zero matrix[i][0].
+    4. Iterate through rest of matrix, nullifying column j if there'sa zero matrix[0][j].
+    5. Nullify the first row and the first column, if necessary (based on values from Step1).
+     */
+    static void setZeros1(int[][] matrix) {
+        boolean rowHasZero = false;
+        boolean colHasZero = false;
+
+        // a Check if first row has a zero
+        for (int j = 0; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                rowHasZero = true;
+                break;
+            }
+        }
+
+        // a Check if first column has a zero
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                colHasZero = true;
+                break;
+            }
+        }
+
+        // b Check for zeros in the rest of the array
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // c Nullify rows based on values in first column
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                nullifyRow(matrix, i);
+            }
+        }
+
+        //d Nullify columns based on values in first row
+        for (int j = 1; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                nullifyColumn(matrix, j);
+            }
+        }
+
+        //Nullify first row
+        if (rowHasZero) {
+            nullifyRow(matrix, 0);
+        }
+
+        //Nullify first column
+        if (colHasZero) {
+            nullifyColumn(matrix, 0);
+        }
+    }
 }
