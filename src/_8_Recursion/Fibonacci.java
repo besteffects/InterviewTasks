1afe10290b374fa1772fa70e0c51180cc684d3e5
Vit <besteffects@gmail.com>
2020-08-07 14:45:49 -0400

    fibonacciCached

--- src/_8_Recursion/Fibonacci.java
+++ src/_8_Recursion/Fibonacci.java
@@ -1,4 +1,5 @@
 package _8_Recursion;
+
 /*
 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, ...
 
@@ -14,12 +15,31 @@ public class Fibonacci {
         long endTime = System.nanoTime();
         long duration = endTime - startTime;
         System.out.println("Recursion with no optimization duration (nanoseconds): " + duration);
+
+        long startTime1 = System.nanoTime();
+        System.out.println(fibonacciCached(10));
+        long endTime1 = System.nanoTime();
+        long duration1 = endTime1 - startTime1;
+        System.out.println("Recursion with optimization duration (nanoseconds): " + duration1);
     }
 
     //slow method with recursion
-    static int fibonacci(int i){
-        if (i==0) return 0;
-        if (i==1) return 1;
-        return fibonacci(i-1)+fibonacci(i-2);
+    static int fibonacci(int i) {
+        if (i == 0) return 0;
+        if (i == 1) return 1;
+        return fibonacci(i - 1) + fibonacci(i - 2);
     }
+
+    //optimized by caching the results of fibonacciCached[i] between calls
+    static int fibonacciCached(int n) {
+        return fibonacciCached(n, new int[n + 1]);
+    }
+    static int fibonacciCached(int i, int[] memo) {
+        if (i == 0 || i == 1) return i;
+        if (memo[i] == 0) {
+            memo[i] = fibonacciCached(i - 1, memo) + fibonacciCached(i - 2, memo);
+        }
+        return memo[i];
+    }
+
 }
